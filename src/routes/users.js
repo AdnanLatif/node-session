4f82ce54d29df28d05526f18fb2daf0749189692
Adnan Latif <alatif.bwp@gmail.com>
2023-06-18 03:09:39 +0500

    Added patch by id and delete by id route.

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,7 +12,8 @@ const authenticateToken = require('../middlewares/authMiddleware');
 
 const usersFile = path.join(__dirname, '..', 'users.json');
 const secretKey = process.env.SECRET_JWT_KEY;
-/* GET users listing. */
+
+// GET users listing.
 router.get('/', (req, res) => {
   try {
     let users = [];
@@ -28,9 +29,10 @@ router.get('/', (req, res) => {
   }
 });
 
+// Create a new user
 router.post('/', async (req, res) => {
   const { username, email, password } = req.body;
-  console.log(req.body);
+
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = { id: uuidv4(), username, email, password: hashedPassword };
@@ -50,6 +52,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Login user
 router.post('/login', (req, res) => {
   const { email, password } = req.body;
   try {
@@ -74,6 +77,24 @@ router.post('/login', (req, res) => {
   }
 });
 
+// Delete the user
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  try {
+    let users = JSON.parse(fs.readFileSync(usersFile));
+    const user = users.find((user) => user.id === id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+    users = users.filter((user) => user.id !== id);
+    fs.writeFileSync(usersFile, JSON.stringify(users, null, 2));
+    res.status(200).json({ message: 'User deleted successfully.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+});
+
 // Protected route to get user's details
 router.get('/profile', authenticateToken, (req, res) => {
   // The user's details can be accessed from req.user
@@ -88,6 +109,7 @@ router.patch('/profile', authenticateToken, (req, res) => {
   const { username } = req.body;
   try {
     let users = JSON.parse(fs.readFileSync(usersFile));
+
     const user = users.find((user) => user.id === id);
     if (!user) {
       return res.status(404).json({ message: 'User not found.' });
@@ -98,6 +120,43 @@ router.patch('/profile', authenticateToken, (req, res) => {
       fs.writeFileSync(usersFile, JSON.stringify(users, null, 2));
       res.status(200).json({ message: 'User updated successfully.' });
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+});
+
+// Update user without authentication
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const { username, email } = req.body;
+  const users = JSON.parse(fs.readFileSync(usersFile));
+  const user = users.find((user) => user.id === id);
+  try {
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+    user.username = username;
+    user.email = email;
+    fs.writeFileSync(usersFile, JSON.stringify(users, null, 2));
+    res.status(200).json({ message: 'User updated successfully.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+});
+
+// GET user by id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const users = JSON.parse(fs.readFileSync(usersFile));
+  const user = users.find((user) => user.id === id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: 'User not found.' });
+  }
+  try {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error.' });
