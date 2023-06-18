7f11d4c1d059e15ba5f070ab5ac1694bb502d57a
Adnan Latif <alatif.bwp@gmail.com>
2023-06-19 04:14:54 +0500

    Added port 5000

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.get('/swagger.json', (req, res) => {
 });
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
