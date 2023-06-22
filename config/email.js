4b2eba6b1fd4ad67fca78f31df698cb4368cad33
Adnan Latif <alatif.bwp@gmail.com>
2023-06-23 03:01:54 +0500

    Updated email configurations.

--- config/email.js
+++ config/email.js
@@ -3,9 +3,12 @@ require('dotenv').config();
 module.exports = {
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    ciphers: 'SSLv3',
   },
 };
